a2689aca09eb8026c0e2e99b0d5a15e098dea66d
dincuy <dincuy23@gmail.com>
2024-09-02 22:00:15 +0700

    0.0.0.0

--- dincuyapp/settings.py
+++ dincuyapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g276au9enry!r2gae(_rk#j#)nsviut_9p8xwboze_z#2ud8ln
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
