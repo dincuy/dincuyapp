d854a02a0d09aec7e441e76eccb40b8b64e43be7
dincuy <dincuy23@gmail.com>
2024-09-04 13:15:30 +0700

    settings.py

--- dincuyapp/settings.py
+++ dincuyapp/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", env('ALLOWED_HOST'),,, "*.a.run.app"]
+ALLOWED_HOSTS = ["0.0.0.0", env('ALLOWED_HOST'), "*.a.run.app"]
 
 
 # Application definition
